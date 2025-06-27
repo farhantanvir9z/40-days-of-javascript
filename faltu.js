3e385363d661faee84397aa49dd4512682cb8df0
Farhan Tanvir <farhantanvir200000@gmail.com>
2025-06-27 17:15:55 +0600

    Update faltu.js

---
+++ faltu.js
@@ -0,0 +1,57 @@
+function createExpenseTracker(userName = "", initialBudget = 0) {
+  let expenses = [];
+  let id = 0;
+  return {
+    name: userName,
+    budget: initialBudget,
+    addExpense: function (amount = 0, catagory = "", description = "") {
+    try {
+      if (isNaN(amount)) {
+        throw new Error("Invalid Input. Amount must be a number.");
+      } else if (amount < 0) {
+        throw new Error("Invalid Amount. Amount must be greater than 0");
+      } else if (catagory === ""){
+        throw new Error("Catagory is empty. Please, write catagory.")
+      } else {
+        expenses.push({
+          id: ++id,
+          amount,
+          catagory,
+          description
+        });
+        console.log(`Now,  You expend ${amount} BDT`)
+        console.log(expenses)
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+    },
+   getTotalExpenses: function () {
+     let arr = [];
+     if (expenses.length === 0) {
+       console.log(arr)
+     } else {
+       expenses.forEach(obj => {
+         arr.push(obj["amount"])
+       });
+       console.log(arr)
+     }
+   },
+   getHighestExpense: function (){
+     
+   },
+   getLowestExpense: function (){
+     
+   },
+  getAllExpenses: function (){
+    
+  },
+  getUserInfo: function () {
+    
+  },
+  updateUserInfo: function () {
+    
+  }
+}
+const user = createExpenseTracker("Farhan", 20000);
+console.log(user);
