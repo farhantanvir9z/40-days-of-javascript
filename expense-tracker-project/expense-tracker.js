2d8e082d196369a8a42386cc0817da34c7e5314b
Farhan Tanvir <farhantanvir200000@gmail.com>
2025-07-11 16:54:26 +0600

    Expence tracker project is complete

--- expense-tracker-project/expense-tracker.js
+++ expense-tracker-project/expense-tracker.js
@@ -1,10 +1,11 @@
+"use strict";
 function createExpenseTracker(userName = "", initialBudget = 0) {
   let expenses = [];
   let id = 0;
   return {
     name: userName,
     budget: initialBudget,
-    addExpense: function (amount = 0, catagory = "", description = "") {
+    addExpense: (amount = 0, catagory = "", description = "") => {
     try {
       if (isNaN(amount)) {
         throw new Error("Invalid Input. Amount must be a number.");
@@ -20,14 +21,13 @@ function createExpenseTracker(userName = "", initialBudget = 0) {
           description
         });
         console.log(`Now,  You expend ${amount} BDT`)
-        console.log(expenses)
       }
     } catch (error) {
       console.error(error.message);
     }
     },
-   getTotalExpenses: function () {
-     let expenseArray = [];
+   getTotalExpenses: () => {
+   let expenseArray = [];
      if (expenses.length === 0) {
        console.log(`Your total expenses is: 0 BDT.`);
      } else {
@@ -41,23 +41,71 @@ function createExpenseTracker(userName = "", initialBudget = 0) {
       console.log(`Your total expenses is: ${totalExpenses} BDT.`)
      }
    },
-   getHighestExpense: function (){
-     
+   getHighestExpense: () => {
+     let expenseArray = [];
+     if (expenses.length === 0) {
+       console.log(`you did nit expend anything yet!`);
+     } else {
+      expenses.forEach(obj => {
+         expenseArray.push(obj["amount"]);
+       });
+     }
+   const highestExpense =   expenseArray.reduce((acc,currentVal) => {
+       acc = (acc < currentVal) ? currentVal : acc;
+       return acc;
+     }, -Infinity);
+   console.log(highestExpense);
    },
-   getLowestExpense: function (){
-     
+   getLowestExpense: () => {
+     let expenseArray = [];
+     if (expenses.length === 0) {
+       console.log(`you did nit expend anything yet!`);
+     } else {
+      expenses.forEach(obj => {
+         expenseArray.push(obj["amount"]);
+       });
+     }
+   const lowestExpense =   expenseArray.reduce((acc,currentVal) => {
+       acc = (acc > currentVal) ? currentVal : acc;
+       return acc;
+     }, Infinity);
+     console.log(lowestExpense);
    },
-  getAllExpenses: function (){
-    
+   getAllExpenses: () => {
+    console.log(expenses)
   },
-  getUserInfo: function () {
-    
+   getUserInfo: function () {
+    console.log({
+      name: this.name,
+      budget: this.budget
+    })
   },
-  updateUserInfo: function () {
+  updateUserInfo: function (userName, budget) {
+   try {
+     if (!userName) {
+       throw new Error("Please, write the username which you want to update with.")
+     } else if (!budget){
+       throw new Error("Please, write the budget which you want to update with.")
+     } else if (isNaN(budget)) {
+       throw new Error("Invalid amount. Please, write a valid amount (Number).");
+     } else if (budget < 0) {
+       throw new Error("Please, write an amount more than 0")
+     } else {
+       this.name = userName,
+       this.budget = budget
+     }
+   } catch (error) {
+     console.error("an error occured:", error.message)
+   }
     
   }
 }
 }
 const user = createExpenseTracker("Farhan", 20000);
+user.addExpense(29,"dress")
+user.addExpense(90,"dress")
+user.addExpense(150,"dress")
+user.addExpense(200,"dress")
+user.addExpense(10,"dress")
 console.log(user);
 
