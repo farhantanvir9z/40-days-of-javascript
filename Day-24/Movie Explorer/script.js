b9e3cc50c43d1ce6cdf8385940f67e2d6dd0fe2c
farhantanvir9z <farhantanvir9z.proton.me>
2025-11-04 14:41:17 +0600

    Movie Explorer project is done!

--- Day-24/Movie Explorer/script.js
+++ Day-24/Movie Explorer/script.js
@@ -1,34 +1,88 @@
+
+// script started
 console.log("Js loaded successfully.");
 
-/*
-first i will add an eventListener on search button which will be triggerred when click event fire.
-Then event handler function will be called named `queryMovie()` => it first resolves the response object then return an array of movies then forEach movie I will call a function named `displayMovie()` => that basically clone the template's content of movie box and append every time the cloned movie box in the movie container.
-*/
-
+// base URL for OMDb (keeps the api key and base params)
 const BASE_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=17ee6f3f";
-const searchButton = document.querySelector(".searchBtn");
 
+// grab the bits from the page
+const searchInputElem = document.querySelector(".search-input");
+const searchButton = document.querySelector(".searchBtn");
+const movieBoxTemplate = document.querySelector(".movie-box-template");
+const movieContainer = document.querySelector(".movie-container");
+
+// run search when the button is clicked
 searchButton.addEventListener("click", queryMovie);
 
+// fetch movies from OMDb using the text from the input
 async function queryMovie() {
-  const searchInputElem = document.querySelector(".searchInput");
-  const searchKeyWord = searchInputElem.value;
-  const response = await fetch(`${BASE_URL}&s=${searchKeyWord}`).then(response => response.json());
-  const movies = response.Search;
-  displayMovie(movies);
-}
-function displayMovie(moviesArray) {
-try {
-  if (!moviesArray) {
-    throw new Error("Movies not found!");
-    return
-  } else {
-    moviesArray.forEach((movie) => {
-      const {Title, Poster, Year} = movie;
-      console.log(Title, Year, Poster)
-    })
+  try {
+    const searchKeyWord = searchInputElem.value;
+    if (searchKeyWord === "") {
+      showError("Please, type movie name");
+    } else {
+    console.log(`Searching for movie: ${searchKeyWord}...`);
+    const response = await fetch(`${BASE_URL}&s=${searchKeyWord}`);
+    const data = await response.json();
+    const movies = data.Search;
+    renderMovies(movies);
+    }
+    
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    showError("Failed to load movies. Please try again.");
   }
-} catch (error) {
-  console.error(error);
 }
+
+// show a list of movies (or do nothing if none)
+function renderMovies(moviesArray) {
+  try {
+    if (!moviesArray) {
+      throw new Error("Movies not found!");
+    } else {
+      movieContainer.innerHTML = "";
+      moviesArray.forEach((movie) => {
+        let { Title, Poster, Year } = movie;
+        Poster = (Poster === "N/A") ? "default-movie-poster.png" : Poster;
+        createMovieBox(Title, Poster, Year);
+      });
+    }
+  } catch (error) {
+       showError("Failed to load movies. Please try again later.");
+    console.error(error);
+  }
+}
+
+function showError(errorMessage) {
+   const errorMessageElem = document.createElement("p");
+    errorMessageElem.classList.add("error");
+    errorMessageElem.innerText = errorMessage;
+    movieContainer.innerHTML = "";
+    movieContainer.appendChild(errorMessageElem);
+}
+// build one movie box from the template and data
+function createMovieBox(Title = "Movie title", Poster = "default-movie-poster.png", Year = 2005) {
+  const cloneMovieBox = movieBoxTemplate.content.cloneNode(true);
+  const moviePoster = cloneMovieBox.querySelector(".poster");
+  const movieTitleElem = cloneMovieBox.querySelector(".movie-title");
+  const movieReleaseYearDisplayElem = cloneMovieBox.querySelector(".movie-release-date");
+
+  moviePoster.setAttribute("src", Poster);
+  movieTitleElem.innerText = Title;
+  movieReleaseYearDisplayElem.innerText = Year;
+
+  const movieWrapperDiv = document.createElement("div");
+  movieWrapperDiv.classList.add("movie-box");
+
+  // put the parts into the wrapper
+  movieWrapperDiv.appendChild(moviePoster);
+  movieWrapperDiv.appendChild(movieTitleElem);
+  movieWrapperDiv.appendChild(movieReleaseYearDisplayElem);
+
+  showMovieBox(movieWrapperDiv);
+}
+
+// add the movie box to the page
+function showMovieBox(movieBoxElem) {
+  movieContainer.appendChild(movieBoxElem);
 }
--- Day-24/task.js
+++
@@ -1,6 +0,0 @@
-async function fetchData() {
-  const URL = `http://www.omdbapi.com/?apikey=17ee6f3f&s=blackhat`;
-  const response = await fetch(URL).then(res => res.json());
-  console.log(response)
-}
-fetchData()
